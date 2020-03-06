c9246d63afb80a5df10e490e75aac8d66e5b5f6b
messmonte <amandamariamonte@gmail.com>
2020-03-06 14:29:09 +0100

    edited get and post to add actions

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 const Sse = require("json-sse");
 const app = express();
 const port = 4000;
@@ -6,12 +7,17 @@ const port = 4000;
 const db = {}; //fake database
 db.messages = []; //fake db table
 
+const corsMiddleWare = cors();
+app.use(corsMiddleWare);
+
 const parser = express.json();
 app.use(parser); //make this before any ENDPOINT!!
 
 const stream = new Sse();
 
 app.get("/stream", (request, response) => {
+  const action = { type: "ALL_MESSAGES", payload: db.messages }; //coming from db
+  stream.updateInit(action); //sending update with all past msgs // change to action
   stream.init(request, response);
 }); //connecting to the stream
 
@@ -19,8 +25,11 @@ app.post("/message", (request, response, next) => {
   const { text } = request.body; //destructuring text
   db.messages.push(text); //push message to db
   response.send(text); //send back what you made
+
+  const action = { type: "NEW_MESSAGE", payload: text };
+
   console.log("db test:", db);
-  stream.send(text); //sending messages over stream
+  stream.send(action); //sending messages over stream
 });
 
 app.listen(port, () => console.log(`Listening on port: ${port}`));
