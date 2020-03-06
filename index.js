b07a919373a5ce99bdc0d1c072196241a7a7dfcd
messmonte <amandamariamonte@gmail.com>
2020-03-06 10:36:19 +0100

    app post + parser

--- index.js
+++ index.js
@@ -2,4 +2,17 @@ const express = require("express");
 const app = express();
 const port = 4000;
 
+const db = {}; //fake database
+db.messages = []; //fake db table
+
+const parser = express.json();
+app.use(parser); //make this before any ENDPOINT!!
+
+app.post("/message", (request, response, next) => {
+  const { text } = request.body; //destructuring text
+  db.messages.push(text); //push message to db
+  response.send(text); //send back what you made
+  console.log("db test:", db);
+});
+
 app.listen(port, () => console.log(`Listening on port: ${port}`));
