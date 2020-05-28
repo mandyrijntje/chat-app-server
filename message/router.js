78461b4cc30371f4aa0c8758c5786c78978baa77
messmonte <amandamariamonte@gmail.com>
2020-05-28 19:00:39 +0200

    Undoing message/router with clean up

--- message/router.js
+++ message/router.js
@@ -1,42 +1,26 @@
 const express = require("express");
+const db = require("../db");
+const stream = require("../stream");
+
 const { Router } = express;
+
 const router = Router();
-const Sse = require("json-sse");
-const stream = new Sse();
 
-const Message = require("./model");
+router.post("/message", (request, response) => {
+  const { text, channel } = request.body;
 
-router.get("/stream", async (request, response, next) => {
-  try {
-    const messages = await Message.findAll();
-    const json = JSON.stringify(messages);
-    stream.updateInit(json);
-    stream.init(request, response);
-  } catch (error) {
-    next(error);
-  }
-});
-router.get("/message", async (request, response, next) => {
-  try {
-    const messages = await Message.findAll();
-    response.send(messages);
-  } catch (error) {
-    next(error);
-  }
-});
+  const message = { text, channel };
 
-router.post("/message", async (request, response, next) => {
-  try {
-    const { body } = request;
-    const { text } = body;
-    const unit = { text };
-    const message = await Message.create(unit);
-    const json = JSON.stringify(message);
-    stream.send(json);
-    response.send(message);
-  } catch (error) {
-    next(error);
-  }
+  db.messages.push(message);
+
+  response.send(message);
+
+  const action = {
+    type: "NEW_MESSAGE",
+    payload: message,
+  };
+
+  stream.send(action);
 });
 
 module.exports = router;
