2baa58315895620333a84f7570f99fdcb8286b5b
messmonte <amandamariamonte@gmail.com>
2020-05-28 19:02:49 +0200

    Undoing index.js with clean up

--- index.js
+++ index.js
@@ -1,17 +1,38 @@
-const cors = require("cors");
 const express = require("express");
-const app = express();
-const port = process.env.PORT || 4000;
+const cors = require("cors");
 const messageRouter = require("./message/router");
 const channelRouter = require("./channel/router");
+const stream = require("./stream");
+const db = require("./db");
 
-const corsMiddleWare = cors();
-app.use(corsMiddleWare);
+const app = express();
 
-const jsonParser = express.json();
-app.use(jsonParser);
+const port = process.env.PORT || 4000;
+
+const corsMiddleware = cors();
+app.use(corsMiddleware);
+
+const parser = express.json();
+app.use(parser);
+
+app.get("/stream", (request, response) => {
+  stream.init(request, response);
+
+  const action = {
+    type: "ALL_MESSAGES",
+    payload: db.messages,
+  };
+  stream.send(action);
+
+  const channelAction = {
+    type: "ALL_CHANNELS",
+    payload: db.channels,
+  };
+
+  stream.send(channelAction);
+});
 
 app.use(messageRouter);
 app.use(channelRouter);
 
-app.listen(port, () => console.log(`Listening on port: ${port}`));
+app.listen(port, () => console.log(`App listening on :${port}`));
