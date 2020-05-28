d64e9a28c39a6e61796e7c033dc66e86f9c6f530
messmonte <amandamariamonte@gmail.com>
2020-05-28 18:17:24 +0200

    Re-created routers in Message router using async

--- message/router.js
+++ message/router.js
@@ -1,20 +1,42 @@
 const express = require("express");
-const db = require("../db");
-const stream = require("../stream");
-
 const { Router } = express;
-
 const router = Router();
+const Sse = require("json-sse");
+const stream = new Sse();
 
-router.post("/message", (request, response, next) => {
-  const { text } = request.body; //destructuring text
-  db.messages.push(text); //push message to db
-  response.send(text); //send back what you made
+const Message = require("./model");
 
-  const action = { type: "NEW_MESSAGE", payload: text };
+router.get("/stream", async (request, response, next) => {
+  try {
+    const messages = await Message.findAll();
+    const json = JSON.stringify(messages);
+    stream.updateInit(json);
+    stream.init(request, response);
+  } catch (error) {
+    next(error);
+  }
+});
+router.get("/message", async (request, response, next) => {
+  try {
+    const messages = await Message.findAll();
+    response.send(messages);
+  } catch (error) {
+    next(error);
+  }
+});
 
-  console.log("db test:", db);
-  stream.send(action); //sending messages over stream
+router.post("/message", async (request, response, next) => {
+  try {
+    const { body } = request;
+    const { text } = body;
+    const unit = { text };
+    const message = await Message.create(unit);
+    const json = JSON.stringify(message);
+    stream.send(json);
+    response.send(message);
+  } catch (error) {
+    next(error);
+  }
 });
 
 module.exports = router;
